2b913dd2d91f68d67c4f0f815cb28ecf8eba960c
Tanguy Maynard <tanguymaynard@proton.me>
2024-01-22 16:35:51 +0100

    Ajout rotation et zoom des elements

--- Scenes/LibreMode/Element/DragNDrop.gd
+++ Scenes/LibreMode/Element/DragNDrop.gd
@@ -16,6 +16,8 @@ func followMouse():
 
 
 func _on_area_2d_input_event(_viewport, event, _shape_idx):
+	
+	# MOUVEMENT
 	Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed:
@@ -23,6 +25,17 @@ func _on_area_2d_input_event(_viewport, event, _shape_idx):
 			selected = true
 		else:
 			selected = false
+			
+	# ZOOM IN / OUT
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_WHEEL_DOWN and scale > Vector2(0.5, 0.5):
+		scale = scale - Vector2(0.1, 0.1)
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_WHEEL_UP and scale < Vector2(6, 6):
+		scale = scale + Vector2(0.1, 0.1)
+		
+	# ROTATION
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT:
+		if event.pressed:
+			rotation_degrees = rotation_degrees + 10
 
 
 func _on_area_2d_mouse_exited():
